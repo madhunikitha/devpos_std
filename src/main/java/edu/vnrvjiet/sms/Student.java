467272cafc1a4ec633e5999e4bb228bd570cb367
madhunikitha2005 <madhunikitha2005@gmail.com>
2025-11-14 20:42:01 +0530

    Initial commit1

--- src/main/java/edu/vnrvjiet/sms/Student.java
+++ src/main/java/edu/vnrvjiet/sms/Student.java
@@ -2,11 +2,20 @@ package edu.vnrvjiet.sms;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
 
 // this is the Domain object
 @Data
 @AllArgsConstructor
+
+
+@Entity
+@Table(name = "student")
 public class Student {
+    @Id
     private Integer id;
     private String name;
     private Integer maths;
